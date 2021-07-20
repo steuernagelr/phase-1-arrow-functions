9438c8fa89990b85a6a9290cc836c18e25e23ce4
Rielly Steuernagel <reiguy@comcast.net>
2021-07-20 12:34:11 -0400

    commit tree 56d4162bab0c962bbd6873a098ab0f0700a6a9d1

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function(numone, numtwo){
+   return numone / numtwo;
+}
+
+divide(2000,100)
+const square = number => number*number
+
+const add = (addone, addtwo) => addone + addtwo
